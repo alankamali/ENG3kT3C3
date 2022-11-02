08f38097c87906e5aa730c3173b0345a8c9e6846
alankamali <alan.kamali97@gmail.com>
2022-11-02 11:09:52 +1100

    fixed timing issue with gates

--- main/main.ino
+++ main/main.ino
@@ -16,7 +16,8 @@
 #define gate_Opened 45 
 #define gate_Closed 90
 
-
+int timee = 0;
+bool check_check = true; 
 
 //IR setup
 int sensor1_pin = 13;
@@ -39,8 +40,8 @@ Servo Servo1;
 Servo Servo2;
 
 int max_count_Marble = 32767; //overflow of the UNO
-unsigned long end_time1;
-unsigned long end_time2;
+int end_time1;
+int end_time2;
 int count_Marble = 0;
 
 
@@ -51,22 +52,22 @@ LiquidCrystal_I2C lcd(0x27, cols, rows); // LCD(address, cols rows);
 
 void setup()
 {
-  	pinMode(LED1, OUTPUT); //LED1
-  	pinMode(LED2, OUTPUT); //LED2
+    pinMode(LED1, OUTPUT); //LED1
+    pinMode(LED2, OUTPUT); //LED2
     pinMode(LED3, OUTPUT); //LED3
-  	pinMode(LED4, OUTPUT); //LED4 
-  	
+    pinMode(LED4, OUTPUT); //LED4 
+    
     //Initialise LED Strip
     strip.begin();
   
-	  //servos setup
+    //servos setup
     //start position of gates is open as agreed with motions teams
-	  Servo1.attach(Servo1_pin);
+    Servo1.attach(Servo1_pin);
     Servo2.attach(Servo2_pin);
     Servo1.write(gate_Opened);
     Servo2.write(gate_Opened);
-	
-  	//IR setup
+  
+    //IR setup
     pinMode(sensor1_pin, INPUT_PULLUP); // sensor1 pin INPUT
     pinMode(sensor2_pin, INPUT_PULLUP); // sensor2 pin INPUT
     pinMode(sensor3_pin, INPUT_PULLUP); // sensor3  pin INPUT
@@ -80,24 +81,30 @@ void setup()
 
 void loop()
 {
+  //check to see if marble has passed last sensor
+  if (check_check == false) {
+    timee += 1;
+    if (timee >= 50) {
+      check_check = true;
+      timee = 0;
+    }
+  }
+
   sensor1_state = digitalRead(sensor1_pin);
   sensor2_state = digitalRead(sensor2_pin);
   sensor3_state = digitalRead(sensor3_pin);
   // Gate 1
+  if (millis() > end_time1){
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED3, HIGH);
+    Servo1.write(gate_Opened);
+  }
   if (sensor1_state == LOW){  //beam has been broken
     //green light off, red light on when beam is broken
     digitalWrite(LED1, HIGH);
     digitalWrite(LED3, LOW);
-
     Servo1.write(gate_Closed);
-    end_time = millis() + 1200;
-
-    if (millis() > end_time){
-      // 
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED3, HIGH);
-      Servo1.write(gate_Opened);
-    }
+    end_time1 = millis() + 1200;
     //green light on, red light off after ball has passed
     digitalWrite(LED1, LOW);
     digitalWrite(LED3, HIGH);
@@ -107,21 +114,17 @@ void loop()
     digitalWrite(LED3, HIGH);
     Servo1.write(gate_Opened);
   }
-
+  if (millis() > end_time2){
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED4, HIGH);
+    Servo2.write(gate_Opened);
+  }
   //Gate 2
   if (sensor2_state == LOW){
     digitalWrite(LED2, HIGH);
     digitalWrite(LED4, LOW);
-
     Servo2.write(gate_Closed);
     end_time2 = millis() + 1200;
-
-    if (millis() > end_time2){
-      // 
-      digitalWrite(LED2, LOW);
-      digitalWrite(LED4, HIGH);
-      Servo2.write(gate_Opened);
-    }
     //green light on, red light off after ball has passed
     digitalWrite(LED2, LOW);
     digitalWrite(LED4, HIGH);
@@ -147,9 +150,12 @@ void loop()
   //XXXXXXX
 
   //Count the number of marble
-  if(sensor3_state == LOW){
+  if(sensor3_state == LOW && check_check == true){
     count_Marble++;
-    lcd.print(count_Marble);
+    check_check = false;
+
+    lcd.clear();
+    lcd.print(count_Marble + " Marbles have passed!");
     if(count_Marble >= max_count_Marble){
       count_Marble = 0;
     }
